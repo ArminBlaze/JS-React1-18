548da85fabdb60fc9b85d612db452f9ac94b96bf
Armin Blaze <box_for_spam@mail.ru>
2019-10-12 01:29:15 +0300

    Вынес Select и DatePicker в отдельный компонент из App

--- src/app.js
+++ src/app.js
@@ -1,41 +1,24 @@
 import React, { Component } from 'react'
 import { findDOMNode } from 'react-dom'
-import Select from 'react-select'
 import ArticleList from './components/article-list'
 import ArticlesChart from './components/articles-chart'
 import articles from './fixtures'
 import UserForm from './components/user-form'
-import DatePicker from 'components/DatePicker';
+import Filters from 'components/Filters';
+
 
 class App extends Component {
-  state = {
-    openItem: null
-  }
 
   render() {
     return (
       <div>
         <UserForm />
-        <Select
-          options={this.options}
-          value={this.state.openItem}
-          onChange={this.handleSelect}
-        />
-        <DatePicker />
+        <Filters articles={articles} />
         <ArticleList articles={articles} ref={this.setArticleListRef} />
         <ArticlesChart articles={articles} />
       </div>
     )
   }
-
-  get options() {
-    return articles.map((article) => ({
-      label: article.title,
-      value: article.id
-    }))
-  }
-
-  handleSelect = (openItem) => this.setState({ openItem })
 
   setArticleListRef = (ref) => {
     console.log('---', ref, findDOMNode(ref))
