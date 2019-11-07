9ba725498e242815335dce7b522e5754b18bd76f
Armin Blaze <box_for_spam@mail.ru>
2019-11-07 23:06:26 +0300

    Добавил Reselect в articles-list

--- src/components/article-list.js
+++ src/components/article-list.js
@@ -3,13 +3,13 @@ import Article from './article/article'
 import accordion from '../decorators/accordion'
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { filterArticles } from 'selectors';
 
 export class ArticleList extends Component {
 
   static propTypes = {
     openItemId: PropTypes.string.isRequired,
     articles: PropTypes.array.isRequired,
-    filters: PropTypes.object.isRequired,
     toggleOpenItem: PropTypes.func.isRequired,
   }
 
@@ -19,9 +19,7 @@ export class ArticleList extends Component {
 
   get body() {
     const { toggleOpenItem, openItemId } = this.props
-    return this.filteredArticles.map((article) => (
-      
-      
+    return this.props.articles.map((article) => (
       <li key={article.id} className='test__articleList__item'>
         <Article
           article={article}
@@ -37,67 +35,18 @@ export class ArticleList extends Component {
     fetchData && fetchData()
   }
 
-
-  get filteredArticles() {
-    const {articles, filters} = this.props;
-    const {selected, dateRange} = filters;
-
-    console.log(filters);
-    
-
-    if(!selected && !dateRange) {
-      return articles;
-    }
-
-    let newArticles;
-
-    if(selected) {
-      newArticles = articles.filter((article) => {
-        return article.id === selected.value
-      })
-    } else {
-      newArticles = articles.slice();
-    }
-    
-    if(dateRange) {
-      newArticles = newArticles.filter((article) => {
-        //тут нужно вычленить дату у каждой статьи
-        let date = parseDate(article.date); //Объект даты
-        // let date2 = Date.parse(article.date);
-        // console.log(+date);
-        // console.log(date2);
-        
-
-        //а потом по ней сравнить попадает ли она в диапазон
-        if(dateRange.from && +date < +dateRange.from) {
-          return false;
-        }
-
-        if(dateRange.to && +date > +dateRange.to) {
-          return false;
-        }
-
-        return true;
-      })
-    }
-  
-
-    return newArticles;
-  }
-
 }
 
 
-function parseDate(date) {
-  const result = date.match(/(\d\d\d\d)-(\d\d)-(\d\d)/)
+// function parseDate(date) {
+//   const result = date.match(/(\d\d\d\d)-(\d\d)-(\d\d)/)
 
-  return new Date(+result[1], +result[2]-1, +result[3]);
-}
+//   return new Date(+result[1], +result[2]-1, +result[3]);
+// }
 
 
 const ArticleListWithAccordion = accordion(ArticleList)
 
 export default connect((state) => ({
-  articles: state.articles,
-  filters: state.filters
+  articles: filterArticles(state),
 }))(ArticleListWithAccordion)
---
+++ src/selectors/index.js
@@ -0,0 +1,57 @@
+import {createSelector} from 'reselect';
+
+export const dateRangeSelector = state => state.filters.dateRange
+export const selectedSelector = state => state.filters.selected
+export const articlesSelector = state => state.articles
+
+
+export const filterArticles = createSelector(
+  dateRangeSelector,
+  selectedSelector,
+  articlesSelector,
+  (dateRange, selected, articles) => {
+    // const {articles, filters} = state;
+    // const {selected, dateRange} = filters;
+
+    console.log(selected, dateRange);
+
+    if(!selected && !dateRange) {
+      return articles;
+    }
+
+    let newArticles;
+
+    if(selected && selected.value) {
+      newArticles = articles.filter((article) => {
+        return article.id === selected.value
+      })
+    } else {
+      newArticles = articles.slice();
+    }
+    
+    if(dateRange) {
+      newArticles = newArticles.filter((article) => {
+        //тут нужно вычленить дату у каждой статьи
+        // let date = parseDate(article.date); //Объект даты
+        let date = Date.parse(article.date);
+        // console.log(+date);
+        // console.log(date2);
+        
+
+        //а потом по ней сравнить попадает ли она в диапазон
+        if(dateRange.from && +date < +dateRange.from) {
+          return false;
+        }
+
+        if(dateRange.to && +date > +dateRange.to) {
+          return false;
+        }
+
+        return true;
+      })
+    }
+
+
+    return newArticles;
+
+})
