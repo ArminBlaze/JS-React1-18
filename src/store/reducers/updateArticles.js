67fc9f71911c60ad9a09dae01958dff1415f82b4
Armin Blaze <box_for_spam@mail.ru>
2019-11-14 16:01:04 +0300

    Немного упростил код добавления комментария к статье

--- src/store/reducers/updateArticles.js
+++ src/store/reducers/updateArticles.js
@@ -24,24 +24,20 @@ export default (state, action) => {
 
     case ADD_COMMENT: {
       //тут код добавления к статье коммента
-      const rawComment = action.payload;
-      const articleId = rawComment.articleId;
-      const commentId = rawComment.id;
+      const articleId = action.payload.articleId;
+      const commentId = action.payload.id;
 
       const oldArticle = articlesState[articleId];
-      const newArticle = {...oldArticle};
 
-      const oldComments = oldArticle.comments;
-      
-      const newComments = oldComments ? [...oldComments, commentId] : [commentId];
+      const newArticle = {
+        ...oldArticle,
+        comments: (oldArticle.comments || []).concat(commentId)
+      };
 
-      newArticle.comments = newComments;
-
-      const newState = {};
-      newState[articleId] = newArticle;
-
-      //взять старую статью. Её клонировать и записать в новый стате.
-      return Object.assign( {}, articlesState, newState)
+      return {
+        ...articlesState,
+        [articleId]: newArticle
+      }
     }
 
     default: 
